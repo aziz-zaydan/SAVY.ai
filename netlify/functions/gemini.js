acbb55eba981207fe55eb0a6f7eb3f2468a8c1ec
aziz-zaydan <140516569+aziz-zaydan@users.noreply.github.com>
2026-03-10 04:38:55 +0100

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -43,7 +43,7 @@ exports.handler = async (event) => {
   return new Promise((resolve) => {
     const options = {
       hostname: "generativelanguage.googleapis.com",
-      path: `/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+      path: `/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
       method: "POST",
       headers: {
         "Content-Type": "application/json",
